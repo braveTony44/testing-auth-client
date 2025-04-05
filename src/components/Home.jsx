1f0280226ab7946bfe95d02d2a78624e6411350d
mrHacker731 <mrhacker11@duck.com>
2025-04-05 19:15:50 +0530

    add link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,22 +2,28 @@ import axios from 'axios'
 import React, { useState } from 'react'
 import { BASE_URL } from '../App'
 import Cookies from 'js-cookie';
+import { Link } from 'react-router-dom';
 
 function Home() {
   const [error,setError] = useState('');
 
   const getUser = async()=>{
-    const cookie = Cookies.get('token');
-    console.log(cookie)
-    const res = await axios.get(`${BASE_URL}/profile`,{
-      withCredentials:true
-    })
-    console.log(res);
+    try {
+      const cookie = Cookies.get('token');
+      console.log(cookie)
+      const res = await axios.get(`${BASE_URL}/profile`,{
+        withCredentials:true
+      })
+      console.log(res.data.user);
+    } catch (error) {
+      setError(error.message)
+    }
   }
   getUser();
     
   return (
     <div>
+      <Link to={'/login'} className='bg-blue' >Login</Link>
         <h2>{error?error:"Welcome to the profile"}</h2>
     </div>
   )
